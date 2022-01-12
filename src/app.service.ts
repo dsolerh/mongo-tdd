0fda5859aa65c538fc3c4fea722e6163dbdba680
dsolerh <dsolerh.cinter95@gmail.com>
2022-01-12 16:05:15 -0500

    add version to the api

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { readFile, stat } from 'fs/promises';
+import { resolve } from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getVersion(): Promise<{ v: string; up: Date }> {
+    const pkgPath = resolve('', 'package.json');
+    const pkgStats = await stat(pkgPath);
+    const pkgFile = await readFile(pkgPath);
+    const pkgString = pkgFile.toString();
+    const pkg = JSON.parse(pkgString);
+    return { v: pkg['version'], up: pkgStats.mtime };
   }
 }
