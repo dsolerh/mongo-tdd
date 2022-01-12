c08c853c8aabebc66fe12b020a791518d7fb6c18
dsolerh <dsolerh.cinter95@gmail.com>
2022-01-12 16:04:14 -0500

    fix mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27018/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
